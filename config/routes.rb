73ecdaa3261f22a98903988b105bf8fa479a4292
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-04-03 04:39:33 +0200

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # As a user, I can see all of my cafes
+  # '/api/v1/cafes'
+  # '/api/v2/cafes'
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :cafes, only: [ :index, :create ]
+    end
+  end
+  # As a user, I can search through all of my cafes
+  # As a user, I can add a cafe
 end
