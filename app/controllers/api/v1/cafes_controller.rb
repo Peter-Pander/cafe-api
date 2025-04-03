970ee953559c90d321d186de04b9bab2b306d04c
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-04-03 05:03:17 +0200

    Add index action to cafes_controller

--- app/controllers/api/v1/cafes_controller.rb
+++ app/controllers/api/v1/cafes_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::CafesController < ApplicationController
+  def index
+    if params[:title].present?
+      # @cafes = Cafe.where(title: params[:title])
+      @cafes = Cafe.where('title ILIKE ?', "%#{params[:title]}%")
+    else
+      @cafes = Cafe.all
+    end
+    # Putting the most recently created cafes first
+    render json: @cafes.order(created_at: :desc)
+  end
 end
