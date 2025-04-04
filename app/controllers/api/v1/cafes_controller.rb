8252904ff2eb11f28309e67c65656611b8fbc9aa
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-04-04 03:03:46 +0200

    Add create action to cafes_controller

--- app/controllers/api/v1/cafes_controller.rb
+++ app/controllers/api/v1/cafes_controller.rb
@@ -9,4 +9,21 @@ class Api::V1::CafesController < ApplicationController
     # Putting the most recently created cafes first
     render json: @cafes.order(created_at: :desc)
   end
+
+  def create
+    @cafe = Cafe.new(cafe_params)
+    if @cafe.save
+      # redirect_to cafe_path(@cafe)
+      render json: @cafe, status: :created
+    else
+      # render the form page, status: :unprocessable_entity
+      render json: { error: @cafe.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def cafe_params
+    params.require(:cafe).permit(:title, :address, :picture, hours: {}, criteria: [])
+  end
 end
